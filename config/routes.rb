9331b5abc9a7de98b78582b150f259c9911b20c5
Alex Kogulko <alexkogulko@gmail.com>
2016-09-23 09:41:18 +0300

    Generate friendly_id

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  resources :categories do
+  resources :categories, shallow: true do
       resources :items
   end
   # You can have the root of your site routed with "root"
