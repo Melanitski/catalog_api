2b7924e4732fa58eb6a3286cf8031503d9676652
Alex Kogulko <alexkogulko@gmail.com>
2016-09-23 09:46:55 +0300

    Use only index and show action for items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < InheritedResources::Base
   respond_to :json
-
+  actions :index, :show
+  
   def index
     category_id = Category.find(params[:category_id]).id
     @items = Item.where(category_id: category_id)
